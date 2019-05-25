607535485563631b8faf506dfe9ac24df32df94a
Tony133 <a.tripodi133@gmail.com>
2019-05-25 22:27:30 +0200

    Update book controller

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -17,11 +17,15 @@ class BookController extends Controller
 
     public function store(BookStoreRequest $request)
     {
-        $book = new Book([
-          'name' => $request->get('name'),
-          'price' => $request->get('price')
-        ]);
-        $book->save();
+        try {
+            $book = new Book([
+                'name' => $request->get('name'),
+                'price' => $request->get('price')
+            ]);
+            $book->save();
+        } catch(\Exception $e) {
+            return response()->json('Error: Book not created!'. $e);
+        }
 
         return response()->json('Successfully book added');
     }
@@ -35,10 +39,14 @@ class BookController extends Controller
 
     public function update(BookUpdateRequest $request, $id)
     {
-        $book = Book::find($id);
-        $book->name = $request->get('name');
-        $book->price = $request->get('price');
-        $book->save();
+        try {
+            $book = Book::find($id);
+            $book->name = $request->get('name');
+            $book->price = $request->get('price');
+            $book->save();
+        } catch(\Exception $e) {
+            return response()->json('Error: Book not updated!'. $e);
+        }
 
         return response()->json('Successfully book updated');
     }
